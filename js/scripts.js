8d99d9536256c99a2a0a85519abe0282804a1886
carloeusebi <carloeusebi@gmail.com>
2023-06-16 15:09:28 +0200

    Added title and picture

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log('JSOK');
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+            pageTitle: 'HELLO WORLD FROM VUE!!',
+            picture: {
+                src: 'https://picsum.photos/900/500',
+                alt: 'a random pic from Lorem Picsum'
+            }
+        };
+    }
+})
+
+app.mount('#root');
